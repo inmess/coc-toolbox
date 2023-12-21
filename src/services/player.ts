5fb220d0f64b936a8b755f14d6aee5faa01e655e
gabrielcheng <in_jaw@outlook.com>
2023-12-21 17:07:13 +0800

    Update API URLs to use HTTP instead of HTTPS

--- src/services/player.ts
+++ src/services/player.ts
@@ -4,7 +4,7 @@ export const get_player = async (player_tag: string, token: string) => {
 
     const encoded_tag = encodeURIComponent(player_tag);
     
-    return await http.fetch(`https://gccld.top/v1/players/${encoded_tag}`, {
+    return await http.fetch(`http://gccld.top/v1/players/${encoded_tag}`, {
         method: 'GET',
         headers: {
             Authorization: token,
--- src/services/verify.ts
+++ src/services/verify.ts
@@ -1,7 +1,7 @@
 import { http } from "@tauri-apps/api";
 
 export const verify_user = async (token: string) => {
-    return await http.fetch(`https://gccld.top/v1/verify`, {
+    return await http.fetch(`http://gccld.top/v1/verify`, {
         method: 'POST',
         headers: {
             Authorization: token,
